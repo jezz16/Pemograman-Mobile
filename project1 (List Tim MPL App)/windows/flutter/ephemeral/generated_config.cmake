d2c42ef54988e91ffcb58eb67454b5f24a619f67
Arfan <99706251+jezz16@users.noreply.github.com>
2024-03-28 15:34:03 +0700

    Add files via upload

---
+++ project1 (List Tim MPL App)/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\App\\dev\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "F:\\`Jezz\\Kuliah\\SMT 6\\MobDev\\flutter_project\\project1_arfan" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\App\\dev\\flutter"
+  "PROJECT_DIR=F:\\`Jezz\\Kuliah\\SMT 6\\MobDev\\flutter_project\\project1_arfan"
+  "FLUTTER_ROOT=D:\\App\\dev\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=F:\\`Jezz\\Kuliah\\SMT 6\\MobDev\\flutter_project\\project1_arfan\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=F:\\`Jezz\\Kuliah\\SMT 6\\MobDev\\flutter_project\\project1_arfan"
+  "FLUTTER_TARGET=F:\\`Jezz\\Kuliah\\SMT 6\\MobDev\\flutter_project\\project1_arfan\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC8wNDgxN2M5OWM5ZmQ0OTU2ZjI3NTA1MjA0ZjdlMzQ0MzM1ODEwYWVkLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=F:\\`Jezz\\Kuliah\\SMT 6\\MobDev\\flutter_project\\project1_arfan\\.dart_tool\\package_config.json"
+)
